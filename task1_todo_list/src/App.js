fbfa99cc006a7896d889a680b0ada3af96795b6c
AryanSrivastava673 <aryansrivastava673@gmail.com>
2022-06-08 00:37:22 +0530

    Created Final App

---
+++ task1_todo_list/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+
+import TodoList from './Components/TodoList'
+
+function App() {
+  return (
+    <div className="todo_list">
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
